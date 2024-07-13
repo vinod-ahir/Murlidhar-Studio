c9bf614309d3685747c5871b32cf7c1ee2ba0373
vinod-ahir <145429796+vinod-ahir@users.noreply.github.com>
2024-07-13 18:15:23 +0530

    slider is in process

--- script.js
+++ script.js
@@ -94,3 +94,39 @@ document.addEventListener("DOMContentLoaded", () => {
   // Start the slideshow
   startSlideshow();
 });
+//Category carousel imaage slider
+$(document).ready(function() {
+  $('#responsive').lightSlider({
+      item:4,
+      loop:true,
+      slideMove:1,
+      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+      speed:600,
+      responsive : [
+        {
+          breakpoint:1300,
+          settings: {
+              item:3,
+              slideMove:1,
+              slideMargin:6,
+            }
+      },
+          {
+              breakpoint:950,
+              settings: {
+                  item:2,
+                  slideMove:1,
+                  slideMargin:6,
+                }
+          },
+          
+          {
+              breakpoint:500,
+              settings: {
+                  item:1,
+                  slideMove:1
+                }
+          }
+      ]
+  });  
+});
