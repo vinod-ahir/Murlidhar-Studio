d17cddfed80b2a89eeb85453099494cc1d6973d9
vinod-ahir <145429796+vinod-ahir@users.noreply.github.com>
2024-08-03 17:43:40 +0530

    side bar corrected

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-// Function to toggle the sidebar's active/unactive state
+// Function to update the menu image source
+function updateMenuIcon(src) {
+  const menuImg = document.getElementById("menu-img");
+  menuImg.src = src;
+}
+
+// Function to toggle the sidebar
 function toggleSidebar() {
   const nav = document.getElementById("nav");
   const menuImg = document.getElementById("menu-img");
@@ -8,20 +14,16 @@ function toggleSidebar() {
     if (nav.classList.contains("active")) {
       updateMenuIcon("icons/menu.svg");
       nav.classList.replace("active", "unactive");
+      document.body.classList.remove("no-scroll");
     } else {
       updateMenuIcon("icons/cross.svg");
       nav.classList.replace("unactive", "active");
+      document.body.classList.add("no-scroll");
     }
     setTimeout(() => {
       menuImg.style.opacity = 1;
     }, 100); // Ensuring the image fades back in after the transition
   }, 100);
-}
-
-// Function to update the menu image source
-function updateMenuIcon(src) {
-  const menuImg = document.getElementById("menu-img");
-  menuImg.src = src;
 }
 
 // Function to close the sidebar
@@ -30,15 +32,21 @@ function closeSidebar() {
   if (nav.classList.contains("active")) {
     updateMenuIcon("icons/menu.svg");
     nav.classList.replace("active", "unactive");
+    document.body.classList.remove("no-scroll");
   }
 }
 
 // Event listener for header click to toggle sidebar
 document.getElementById("header").addEventListener("click", toggleSidebar);
 
-// Close sidebar when clicking outside of nav and header
-window.addEventListener("click", (event) => {
-  const nav = document.getElementById("nav");
+// Close sidebar when clicking or touching outside of nav and header
+document.addEventListener("click", (event) => {
+  if (!event.target.closest("#nav") && !event.target.closest("#header")) {
+    closeSidebar();
+  }
+});
+
+document.addEventListener("touchstart", (event) => {
   if (!event.target.closest("#nav") && !event.target.closest("#header")) {
     closeSidebar();
   }
@@ -47,26 +55,17 @@ window.addEventListener("click", (event) => {
 // Close sidebar when scrolling while nav is open
 window.addEventListener("scroll", () => {
   const nav = document.getElementById("nav");
-  if (nav.classList.contains("open")) {
+  if (nav.classList.contains("active")) {
     closeSidebar();
   }
 });
 
-// Close sidebar on touch outside of nav in touch devices
-document.addEventListener("touchstart", (event) => {
-  const nav = document.getElementById("nav");
-  if (!event.target.closest("#nav") && !event.target.closest("#header")) {
-    closeSidebar();
-  }
-});
-
-// Close sidebar on scroll outside of nav in touch devices
 document.addEventListener("touchmove", (event) => {
-  const nav = document.getElementById("nav");
   if (!event.target.closest("#nav") && !event.target.closest("#header")) {
     closeSidebar();
   }
 });
+
 // Slideshow
 document.addEventListener("DOMContentLoaded", () => {
   const slides = document.querySelectorAll(".slideshow #slideshowimg");
@@ -113,13 +112,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Add click and touch event listeners to control buttons
   ctrlButtons.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener("click", () => {
       slideIndex = i;
       showSlide(slideIndex);
       resetTimer();
     });
 
-    btn.addEventListener('touchstart', (e) => {
+    btn.addEventListener("touchstart", (e) => {
       e.preventDefault(); // Prevents default touch behavior
       slideIndex = i;
       showSlide(slideIndex);
@@ -129,29 +128,28 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Category carousel image slider
-$(document).ready(function() {
-  $('#responsive').lightSlider({
-      item:3,
-      slideMove:3,
-      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-      speed:400,
-      responsive : [
-          {
-              breakpoint:1024,
-              settings: {
-                  item:2,
-                  slideMove:2,
-                  slideMargin:6,
-                }
-          },
-          {
-              breakpoint:480,
-              settings: {
-                  item:1,
-                  slideMove:1
-                }
-          }
-      ]
-  });  
+$(document).ready(function () {
+  $("#responsive").lightSlider({
+    item: 3,
+    slideMove: 3,
+    easing: "cubic-bezier(0.25, 0, 0.25, 1)",
+    speed: 400,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          item: 2,
+          slideMove: 2,
+          slideMargin: 6,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+        },
+      },
+    ],
+  });
 });
-
